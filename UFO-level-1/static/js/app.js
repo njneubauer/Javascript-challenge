3601b616e890dae3065b33a359f2a15acc49fb7b
njneubauer <njneubauer@gmail.com>
2021-03-29 19:24:29 -0500

    updated code to stop re-rendering data for same query submitted back to back

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,8 +4,34 @@ var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputField = d3.select("#datetime");
 
+// Default Table function
+function tableDefault(){
+tableData.forEach(function(sighting){
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]){
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+};
+
+// Call default table with page load
+tableDefault();
+
+// Filter table function. If input blank while submitting load default table. 
+// If default table is already loaded
 function tableFilter(newInput){
-    if (oldInput() !== newInput){
+    console.log(`new: ${newInput}`)
+    console.log(`old: ${oldInput()}`)
+    if (newInput === "" && oldInput() === "1/13/2010"){
+        console.log("Default table already loaded")
+    }
+    else if (newInput === ""){
+        console.log("loaded default table");
+        tbody.html("");
+        tableDefault();
+    }
+    else if (oldInput() !== newInput){
         tbody.html("");
     dateFilter = tableData.filter(data => data.datetime == newInput);
     dateFilter.forEach(function(sighting){
@@ -16,17 +42,18 @@ function tableFilter(newInput){
             cell.text(value);
         });
     });
-    }
+    }   
     else if (oldInput() === newInput){
         console.log("Input submitted has not changed from last query")
-    }; 
-    
+    };
 };
 
+// function that returns last date in the data table.
+// If the first date in the table matches the submitted input no need to render table again.
 function oldInput(){
     var tdEmpty = d3.select("tr td").empty();
     if (tdEmpty === false) {
-        var td = d3.select("tr td").node("innerhtml").textContent;
+        var td = d3.select("tr:last-child td:first-child").node("innerhtml").textContent;
         return td;
     }
     else {
@@ -34,7 +61,16 @@ function oldInput(){
     };
 };
 
+// Sends date from inputField to tableFilter function
+function clickHandle() {
+    console.log("Button clicked");
+    var newInput = inputField.property("value");
+    tableFilter(newInput);
+};
 
+button.on("click", clickHandle);
+
+// Allows user to use "Enter" key to submit a date
 inputField.on("keyup",function(){
     if (d3.event.keyCode == 13) {
         console.log("Enter key event");
@@ -46,13 +82,7 @@ inputField.on("keyup",function(){
 });
 
 
-function clickHandle() {
-    console.log("Button clicked");
-    var newInput = inputField.property("value");
-    tableFilter(newInput);
-};
 
-button.on("click", clickHandle);
 
 
 
